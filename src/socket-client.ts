ee1854d4a581203b3ebe67b5be1834fcd937bda3
marcelopuyo <103154007+marcelopuyo@users.noreply.github.com>
2024-11-26 23:32:41 -0300

    final seccion 15

---
+++ src/socket-client.ts
@@ -0,0 +1,77 @@
+import { Manager, Socket } from "socket.io-client"
+
+let socket: Socket;
+
+export const connectToServer = (token: string) => {
+
+  const manager = new Manager('http://localhost:3000/socket.io/socket.io.js', {
+    extraHeaders: {
+      authentication: token
+    }
+  });
+
+  socket?.removeAllListeners();
+  
+  socket = manager.socket('/');
+
+  addListeners();
+}
+
+const addListeners = () => {
+
+  const serverStatusLabel = document.querySelector('#server-status');
+  const clientsUl = document.querySelector('#clients-ul');
+  const mesForm = document.querySelector<HTMLFormElement>('#message-form')!;
+  const mesInput = document.querySelector<HTMLInputElement>('#message-input')!;
+  const messagesUl = document.querySelector<HTMLUListElement>('#messages-ul')!;
+
+  socket.on('connect', () => {
+    serverStatusLabel!.innerHTML = 'Connected';
+  })
+
+  socket.on('disconnect', () => {
+    serverStatusLabel!.innerHTML = 'Disconnected';
+  })
+
+  socket.on('clients-updated', (clients: string[]) => {
+    let clientsHtml = '';
+
+    clients.forEach((clientId) => {
+      clientsHtml += `
+      <li>${ clientId }</li>
+      `
+    });
+
+    clientsUl!.innerHTML = clientsHtml;
+  })
+
+  mesForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (mesInput.value.trim().length <= 0) return;
+
+    socket.emit('message-from-client', {
+      id: 'MP',
+      message: mesInput.value
+    });
+
+    mesInput.value = '';
+
+  })
+
+  socket.on('message-from-server', (payload: {fullName: string, message: string}) => {
+    const newMessage = `
+    <li>
+      <strong>${ payload.fullName }</strong>
+      <span>${ payload.message }</span>
+    </li>
+    `;
+
+    const li = document.createElement('li');
+    li.innerHTML = newMessage;
+    messagesUl.append(li);
+
+  });
+
+
+}
